0d800b5d57aab59cb30cf5a73ff360614c9e1bc3
Martin Barreto <martin@xmartlabs.com>
2014-02-26 11:17:00 -0200

    Initial Commit

---
+++ XLMailBoxContainer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XLMailBoxContainer
+//
+//  Created by Martin Barreto on 2/26/14.
+//  Copyright (c) 2014 Xmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XLMailBoxContainer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XLMailBoxContainer
+//
+//  Created by Martin Barreto on 2/26/14.
+//  Copyright (c) 2014 Xmartlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
